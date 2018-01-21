686b072d0397fa9b811e6277eab8f46cdd890e1a
activecreator <yash0312@hotmail.com>
2018-01-21 10:03:28 -0500

    adjusted x and y cordinatz
    bye

--- Breakout/breakout.js
+++ Breakout/breakout.js
@@ -1,27 +1,19 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-var x = 250;
-var y = 150;
-
-ctx.fillStyle = 'red';
-ctx.fillRect(50, 200, 80, 80);
-ctx.stroke();
-
-ctx.fillStyle = 'blue';
-ctx.fillRect(70, 10, 80, 30);
-ctx.stroke();
+var x = canvas.width/2;
+var y = canvas.height-60;
 
 function draw() {
-    ctx.clearRect(300, 300, canvas.width, canvas.height);
-    x=x+1;
-    y=y+1;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    x=x-1;
+    y=y-1;
     ctx.beginPath();
-    ctx.arc(x, y ,50 ,0 ,2*Math.PI);
+    ctx.arc(x, y, 60 ,0 ,2*Math.PI);
     ctx.fillStyle = "green";
     ctx.fill();
     ctx.stroke();
     ctx.closePath();
     
 }
-setInterval(draw, 100);
+setInterval(draw, 10);
