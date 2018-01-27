43feef240d26363d4574c9903cccc5173e87a8dc
activecreator <yash0312@hotmail.com>
2018-01-27 09:22:32 -0500

    separated vars

--- Breakout/breakout.js
+++ Breakout/breakout.js
@@ -3,22 +3,27 @@ var ctx = canvas.getContext('2d');
 
 var x = canvas.width/2;
 var y = canvas.height-60;
-
 var step = 1;
+var ballRadius = 10;
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-    step = step+ 0.5
-    x = x- step
-    y = y- step
-    
+
+function drawBall() {
+   
     ctx.beginPath();
-    ctx.arc(x, y, 60 ,0 ,2*Math.PI);
+    ctx.arc(x, y, ballRadius ,0 , 2*Math.PI);
     ctx.fillStyle = "green";
     ctx.fill();
     ctx.stroke();
     ctx.closePath();
     
+}
+    
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    //step = step + 0.5
+    x = x- step
+    y = y- step
+      
 }
 setInterval(draw, 10);
