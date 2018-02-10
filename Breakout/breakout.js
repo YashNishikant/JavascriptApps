ca03e220467ae4d47e469171edde24ba02c879dc
activecreator <yash0312@hotmail.com>
2018-02-10 11:01:58 -0500

    added random color
    hipoop

--- Breakout/breakout.js
+++ Breakout/breakout.js
@@ -6,13 +6,13 @@ var y = canvas.height-60;
 var stepX = 1;
 var stepY = 1;
 var ballRadius = 10;
-
+var anycolor = randomColor();
 
 function drawBall() {
    
     ctx.beginPath();
     ctx.arc(x, y, ballRadius ,0 , 2*Math.PI);
-    ctx.fillStyle = "green";
+    ctx.fillStyle = anycolor;
     ctx.fill();
     ctx.stroke();
     ctx.closePath();
@@ -20,24 +20,29 @@ function drawBall() {
 }
     
 function draw() {
-    
     canvas.width=window.innerWidth-20;
     canvas.height=window.innerHeight-20;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     
-    if ( (x<11) || (x>canvas.width - ballRadius) )
+    if ( (x<ballRadius) || (x>canvas.width - ballRadius) )
     {
-        stepX = -1 * stepX;
+        stepX = -1 * stepX; 
+        randomColor();
     }
     
-    if ( (y<11) || (y>canvas.height - ballRadius) )
+    if ( (y<ballRadius) || (y>canvas.height - ballRadius) )
     {
-        stepY = -1 * stepY;    
+        stepY = -1 * stepY;  
+        randomColor();
     }
     
     x = x- stepX
     y = y- stepY
 
+    
+    console.log(anycolor)
+    
+    
 }
 setInterval(draw, 10);
